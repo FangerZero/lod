92a15f73700d8135628dc95e9842d54e8b253a05
FangerZero <FangerZero@hotmail.com>
2022-12-30 14:55:33 -0600

    Update Map Page and added Map image

--- pages/game/map.js
+++ pages/game/map.js
@@ -1,8 +1,12 @@
+import Image from 'next/image';
+import Meta from '../../components/layout/Meta';
+import endiness from '../../public/images/game/endiness.webp';
 
 export default function Map() {
     return (
       <div>
-          Map
+          <Meta title="Endiness Map" description="Explore the Endiness continent." />
+          <Image src={endiness} width="1000"/>
       </div>
     )
   }
