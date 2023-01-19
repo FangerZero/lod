2e405498339439cfc94f1b99b45ffd3569862cfd
Fanger Zero <FangerZero@hotmail.com>
2023-01-19 00:47:55 -0600

    Changed from File to FireBase

--- pages/game/characters/index.js
+++ pages/game/characters/index.js
@@ -1,11 +1,13 @@
 import Link from 'next/link';
 import Image from 'next/image';
-import fs from 'fs/promises';
-import path from 'path';
 import Meta from '../../../components/layout/Meta';
+
+import { collection, query, limit, getDocs, where, orderBy, startAt} from "firebase/firestore";
+import { db } from '../../../lib/firebase';
 
 export default function Characters(props) {
   const { characters } = props;
+  console.log('characters: ', characters)
 
   return (
     <>
@@ -22,13 +24,32 @@ export default function Characters(props) {
 }
 
 export async function getStaticProps() {
-  const filePath = path.join(process.cwd(), 'components','game','data','characters.json');
-  const jsonData = await fs.readFile(filePath);
-  const characters = JSON.parse(jsonData);
+  console.log('meow');
+  
+  let queryIndex = 0;
+  let queryCount = 0;
+  const queryLimit = 25;
+  const characters = [];  
 
+  do {
+    // Query Database 
+    let charQuery = query(collection(db, "characters"), orderBy("name"), startAt(queryIndex), limit(queryLimit));
+    const charDocs = await getDocs(charQuery);
+
+    console.log('length: ', charDocs.docs.length);
+    queryCount = charDocs.docs.length;
+    queryIndex+= queryLimit;
+
+    charDocs.forEach((doc) => {
+      const data = doc.data();
+      characters.push({ link: data.link, name: data.name, "profile-img": data['profile-img']});
+    });
+  } while (queryCount === queryLimit);
+  
   return {
       props: {
         characters: characters
       },
   };
 }
+
